25facd7f5bee2760cca9ee8c436a83b83e64850c
Y.Maeshima <ymzlgcranch@gmail.com>
2020-06-11 20:06:47 +0900

    shuffleメソッドを実装

--- src/services/PlayerAllocator.ts
+++ src/services/PlayerAllocator.ts
@@ -1,10 +1,29 @@
 import Player from "../models/player";
 
+/**
+ * 常に4人以上のプレイ順を決定する
+ */
 class PlayerAllocator {
-  private readonly queue: Player[] = [];
+  public readonly queue: Player[] = [];
+  private list: Player[] = [];
+  private readonly displayNumber = 4;
 
   public join(player: Player): void {
     this.queue.push(player);
+  }
+
+  /**
+   *
+   */
+  public shuffle(): void {
+    const max = this.queue.length;
+    while (this.list.length < this.displayNumber) {
+      const arr = this.queue.slice();
+      while (arr.length > 0) {
+        const target = arr.splice(Math.floor(Math.random() * max), 1);
+        this.list.push(target.shift());
+      }
+    }
   }
 }
 
